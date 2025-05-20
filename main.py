b11fa29d749553342e256da1009f05e13bd87bef
严相荣 <yanxiangrong@huya.com>
2025-05-20 10:16:49 +0800

    add metrics collection functionality using Prometheus client

---
+++ main.py
@@ -0,0 +1,15 @@
+import requests
+from prometheus_client.parser import text_string_to_metric_families
+
+# Windows Exporter 的地址和端口，默认是9182
+EXPORTER_URL = "http://localhost:9182/metrics"
+
+def collect_metrics():
+    resp = requests.get("http://your_exporter_ip:9182/metrics")
+    resp.raise_for_status()
+    metrics = {}
+    for family in text_string_to_metric_families(resp.text):
+        for sample in family.samples:
+            name, labels, value = sample
+            metrics.setdefault(name, []).append((labels, value))
+    return metrics
