3fff7d7932242f7f63501e1b5ba7f16af4f1fa3d
严相荣 <yanxiangrong@huya.com>
2025-05-20 10:20:45 +0800

    refactor collect_metrics function to accept URL as a parameter

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ from prometheus_client.parser import text_string_to_metric_families
 # Windows Exporter 的地址和端口，默认是9182
 EXPORTER_URL = "http://localhost:9182/metrics"
 
-def collect_metrics():
-    resp = requests.get("http://your_exporter_ip:9182/metrics")
+def collect_metrics(url:str):
+    resp = requests.get(url)
     resp.raise_for_status()
     metrics = {}
     for family in text_string_to_metric_families(resp.text):
